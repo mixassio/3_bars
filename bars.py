46c04fcc5def4e894cf45b8fb4e151a5b5aa7f44
mixassio <mixail.bondarev@gmail.com>
2017-03-21 13:14:55 +0000

    исправил замечание Валли о snake_case

--- bars.py
+++ bars.py
@@ -7,7 +7,7 @@ import math
 import sys
 import argparse
 
-def createParser ():
+def Create_parser ():
     parser = argparse.ArgumentParser()
     parser.add_argument ('filepath')
     parser.add_argument ('-min', '--minimum', action='store_const', const=True)
@@ -49,7 +49,7 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    parser = createParser()
+    parser = Create_parser()
     namespace = parser.parse_args()
     data_bars = load_data(namespace.filepath)
     
