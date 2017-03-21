31d3c902c9f4625d542222af16caab6a7804dde1
mixassio <mixail.bondarev@gmail.com>
2017-03-21 12:45:59 +0000

    Доделал все замечания

--- bars.py
+++ bars.py
@@ -1,73 +1,44 @@
-"""
-The module is designed to work with a resource https://data.mos.ru/opendata/7710881420-bary.
-The module allows you to find the smallest bar using the function get_biggest_bar(data),
-the largest bar, using the function get_biggest_bar(data)
-and the nearest bar using coordinates, using the function get_closest_bar(data, longitude, latitude).
-To work the module you need a file ".json" that you need to download from https://data.mos.ru/opendata/7710881420-bary.
-Use the function load_data(filepath) to load data.
-"""
+
 import json
 import codecs
 import os
 import chardet
 import math
+import sys
+import argparse
 
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('filepath')
+    parser.add_argument ('-min', '--minimum', action='store_const', const=True)
+    parser.add_argument ('-max', '--maximum', action='store_const', const=True)
+    parser.add_argument ('-n', '--nearest', nargs=2) 
+    return parser
+    
 
 def load_data(filepath):
-    """
-    Function is designed to process the downloaded file into a format json
-    :param filepath: Path to file on your computer
-    :return: json-object
-    """
     if os.path.exists(filepath):
-        char_type = chardet.detect(open(filepath, "rb").read())['encoding']
-        with codecs.open(filepath, 'rb', encoding=char_type) as fh:
-            return json.load(fh)
+        with open(filepath, 'rb') as file_json:
+            char_type = chardet.detect(file_json.read())['encoding']
+        with codecs.open(filepath, 'rb', encoding=char_type) as file_json:
+            return json.load(file_json)
 
 
 def get_biggest_bar(data):
-    """
-    The function finds the biggest bar. The number of seats is considered
-    :param data: json-object with bars
-    :return: Message with the name of the bar and address
-    """
-    max_seats_counts = 0
-    num_bar = 0
-    for bar_id, bar in enumerate(data):
-        if bar['SeatsCount'] > max_seats_counts:
-            max_seats_counts = bar['SeatsCount']
-            num_bar = bar_id
-    return data[num_bar]
+    return max(data, key=lambda x:x['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    """
-    The function finds the smallest bar. The number of seats is considered
-    :param data: json-object with bars
-    :return: Message with the name of the bar and address
-    """
-    min_seats_counts = 1000
-    num_bar = 0
-    for bar_id, bar in enumerate(data):
-        if 0 < bar['SeatsCount'] < min_seats_counts:
-            min_seats_counts = bar['SeatsCount']
-            num_bar = bar_id
-    return data[num_bar]
+    return min(data, key=lambda x:x['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
-    """
-    The function finds the nearest bar at the current coordinates
-    :param data: json-object with bars
-    :param longitude: current longitude coordinates
-    :param latitude: current latitude coordinates
-    :return: Message with the name of the bar and address
-    """
     mx = 0
     my = 0
     min_dist_to_bar = 1000.
     num_bar = 0
-    for bar_id, bar in enumerate(data):
+    for bar_id, bar in enumerate(data): # bar_id я использую для итерирования по списку, у меня сразу есть номер словаря искомого
+        print(bar_id, bar['global_id'])
         mx = abs(latitude - float(bar['Latitude_WGS84']))
         my = abs(longitude - float(bar['Longitude_WGS84']))
         distance_to_bar = math.sqrt(mx**2 + my**2)
@@ -78,15 +49,24 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    data_bars = load_data(namespace.filepath)
+    
+    if namespace.maximum:
+        big_bar = get_biggest_bar(data_bars)
+        print('Самый большой бар - {}, по адресу {}'.format(big_bar['Name'], big_bar['Address']))
+    if namespace.minimum:
+        small_bar = get_smallest_bar(data_bars)
+        print('Самый маленький бар - {}, по адресу {}'.format(small_bar['Name'], small_bar['Address']))
+    if namespace.nearest:
+        nearest_bar = get_closest_bar(data_bars, float(namespace.nearest[0]), float(namespace.nearest[0]))
+        print('Ближайший бар - {}, по адресу {}'.format(nearest_bar['Name'], nearest_bar['Address']))
+        
+    if namespace.maximum==None and namespace.minimum==None and namespace.nearest==None:
+        big_bar = get_biggest_bar(data_bars)
+        print('Самый большой бар - {}, по адресу {}'.format(big_bar['Name'], big_bar['Address']))
+        small_bar = get_smallest_bar(data_bars)
+        print('Самый маленький бар - {}, по адресу {}'.format(small_bar['Name'], small_bar['Address']))
+        print('Для поиска ближайшего бара введите координаты')
 
-    data_bars = load_data('data.json')
-    print(data_bars)
-
-    big_bar = get_biggest_bar(data_bars)
-    print('Самый большой бар - {}, по адресу {}'.format(big_bar['Name'], big_bar['Address']))
-
-    small_bar = get_smallest_bar(data_bars)
-    print('Самый маленький бар - {}, по адресу {}'.format(small_bar['Name'], small_bar['Address']))
-
-    nearest_bar = get_closest_bar(data_bars, 37.618762, 55.625307)
-    print('Ближайший бар - {}, по адресу {}'.format(nearest_bar['Name'], nearest_bar['Address']))
